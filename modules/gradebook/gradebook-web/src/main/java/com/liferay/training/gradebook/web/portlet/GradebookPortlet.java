94284b167a2c1ed84c93755002950f89a5651aa4
Kosalvireak Khoeun <kosalvireak.khoeun@khalibre.com>
2022-10-20 17:14:29 +0700

    add guestbook

---
+++ modules/gradebook/gradebook-web/src/main/java/com/liferay/training/gradebook/web/portlet/GradebookPortlet.java
@@ -0,0 +1,33 @@
+package com.liferay.training.gradebook.web.portlet;
+
+import com.liferay.training.gradebook.web.constants.GradebookPortletKeys;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author kosalvireak
+ */
+@Component(
+        immediate = true,
+        property = {
+                "com.liferay.portlet.display-category=category.training",
+                "com.liferay.portlet.css-class-wrapper=gradebook-portlet",
+                "com.liferay.portlet.header-portlet-css=/css/main.css",
+                "com.liferay.portlet.instanceable=false",
+                "javax.portlet.display-name=GRADEBOOK",
+                "javax.portlet.init-param.template-path=/",
+                "javax.portlet.init-param.view-template=/view.jsp",
+                "javax.portlet.name=" + GradebookPortletKeys.GRADEBOOK,
+                "javax.portlet.resource-bundle=content.Language",
+                "javax.portlet.security-role-ref=power-user,user",
+                "javax.portlet.init-param.add-process-action-success-action=false"
+        },
+        service = Portlet.class
+)
+
+public class GradebookPortlet extends MVCPortlet {
+}
