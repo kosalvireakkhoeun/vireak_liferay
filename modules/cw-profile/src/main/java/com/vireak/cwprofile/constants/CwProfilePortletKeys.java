451cd989287a1007c2bdfdd648a07d4035522db6
Kosalvireak Khoeun <kosalvireak.khoeun@khalibre.com>
2022-10-25 17:36:56 +0700

    add DisplayName

--- modules/cw-profile/src/main/java/com/vireak/cwprofile/constants/CwProfilePortletKeys.java
+++ modules/cw-profile/src/main/java/com/vireak/cwprofile/constants/CwProfilePortletKeys.java
@@ -6,5 +6,5 @@ package com.vireak.cwprofile.constants;
 public class CwProfilePortletKeys {
 
 	public static final String CWPROFILE ="CwProfilePortlet";
-
+	public static final String DisplayName = "cwprofile";
 }
--- modules/cw-profile/src/main/java/com/vireak/cwprofile/portlet/CwProfilePortlet.java
+++ modules/cw-profile/src/main/java/com/vireak/cwprofile/portlet/CwProfilePortlet.java
@@ -17,7 +17,7 @@ import org.osgi.service.component.annotations.Component;
 		"com.liferay.portlet.display-category=category.social",
 		"com.liferay.portlet.header-portlet-css=/css/main.css",
 		"com.liferay.portlet.instanceable=false",
-		"javax.portlet.display-name=CwProfile",
+		"javax.portlet.display-name=" + CwProfilePortletKeys.DisplayName,
 		"javax.portlet.init-param.template-path=/",
 		"javax.portlet.init-param.view-template=/view.jsp",
 		"javax.portlet.name=" + CwProfilePortletKeys.CWPROFILE,
